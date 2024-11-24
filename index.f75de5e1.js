baadd3161766a3b0e406a8246edaa7e7c83c6ae1
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-11-24 21:08:30 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,26 @@
+'use strict';
+// head and foot
+const thead = document.querySelector('thead');
+const tfoot = document.querySelector('tfoot');
+[
+    thead,
+    tfoot
+].forEach((el)=>{
+    const lastElement = el.querySelector('tr').lastElementChild;
+    const title = document.createElement('th');
+    title.innerText = 'Position';
+    lastElement.insertAdjacentElement('beforebegin', title);
+});
+// body
+const tbody = document.querySelector('tbody');
+const people = [
+    ...tbody.children
+];
+people.forEach((person)=>{
+    const position = [
+        ...person.children
+    ][1].cloneNode(true);
+    person.lastElementChild.insertAdjacentElement('beforebegin', position);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
